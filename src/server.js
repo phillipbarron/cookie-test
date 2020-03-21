e56fd177b39e3d9f41f0790514d7711c25459a51
Phill Barron <phillipbarron242@gmail.com>
2020-03-21 14:10:22 +0000

    addesession cookied

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const cookieSession = require('cookie-session')
+
 const app = express()
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['foo', 'bar'],
+    maxAge: 24 * 60 * 60 * 1000
+  }))
+
 const port = 3000
 
-app.get('/', (req, res) => res.send({foo: 'bar'}));
+app.get('/', (req, res) => {
+      // Update views
+  req.session.views = (req.session.views || 0) + 1
+
+  // Write response
+  res.end(req.session.views + ' views');
+});
 
 app.listen(port, () => console.log(`Cookie tester port ${port}!`));
